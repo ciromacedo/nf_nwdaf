3cadb2e00a1c6b8cc00052e417769e6fa6f33733
ciromacedo <ciromacedo@gmail.com>
2021-02-08 08:50:01 -0300

    add changes

--- base/db.go
+++ base/db.go
@@ -6,35 +6,45 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
+	"time"
 )
 
-func GetConnection() *mongo.Database {
-	clientOptions := options.Client().ApplyURI( GetMongoDBUri())
-	client, err := mongo.Connect(context.TODO(), clientOptions)
+func GetConnection() (*mongo.Database, *mongo.Client, context.Context) {
+
+	client, err := mongo.NewClient(options.Client().ApplyURI(GetMongoDBUri()))
 	if err != nil {
 		log.Fatal(err)
 	}
-	// Check the connection
-	err = client.Ping(context.TODO(), nil)
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
 	if err != nil {
 		log.Fatal(err)
 	}
 	conn := client.Database(GetDBName())
-	return conn
+	return conn, client, ctx
 }
 
 var CollectionNames []string
 func GetCollectionsName()[]string {
-	result, err := GetConnection().ListCollectionNames(context.TODO(), bson.D{})
+	db, client, ctx := GetConnection()
+	result, err := db.ListCollectionNames(context.TODO(), bson.D{})
 	if err != nil {
 		log.Fatal(err)
 	}
 	for _, coll := range result {
 		CollectionNames = append(CollectionNames, coll)
 	}
+	CloseConnection(client, ctx)
 	return CollectionNames
 }
 
 func GetNumberOfRecordsInCollection(name string) (int64, error) {
-	return GetConnection().Collection(name).EstimatedDocumentCount(context.TODO())
+	db, client, ctx := GetConnection()
+	qtd, error := db.Collection(name).EstimatedDocumentCount(context.TODO())
+	CloseConnection(client, ctx)
+	return qtd, error
+}
+
+func CloseConnection(cli*mongo.Client, ctx context.Context){
+	cli.Disconnect(ctx)
 }
--- debug.go
+++ debug.go
@@ -2,11 +2,34 @@ package main
 
 import (
 	"log"
+	"nf_nwdaf/base"
 	"os"
 )
 
 func main() {
 	log.Println("init debug")
 	log.Println(os.Getenv("DB_URI"))
-	log.Println("end debug")
+	log.Println(base.GetMongoDBUri())
+    log.Println(base.GetNumberOfRecordsInCollection("urilist"))
+/*	client, err := mongo.NewClient(options.Client().ApplyURI(os.Getenv("DB_URI")))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	result, err := client.Database(base.GetDBName()).ListCollectionNames(context.TODO(), bson.D{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, coll := range result {
+		log.Println(coll)
+	}
+
+	defer client.Disconnect(ctx)
+*/
+
 }
+
